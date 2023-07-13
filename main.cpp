f5a9a5fb722a0a265af9a0fb6b4cac9293454f23
Mahdi-2mj <123583354+Mahdi-2mj@users.noreply.github.com>
2023-07-13 21:38:35 +0330

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <QtWidgets>
+#include <QTimer>
+#include "doodler.h"
+#include "platform.h"
+#include "random.h"
+#include <QApplication>
+#include <QStringConverter>
+#include <QTime>
+#include <QSplashScreen>
+#include "doodleview.h"
+
+int main(int argc, char* argv[]) {
+
+    QApplication  app(argc, argv);
+    QPixmap pix(":/image/scr.jpg");
+    QSplashScreen *splash = new QSplashScreen;
+    splash->setGeometry(0,0,800,1000);
+    splash->setPixmap(pix);
+    splash->show();
+    DoodleView view;
+    view.show();
+    splash->finish(&view);
+    return app.exec();
+}
